6a47c0a7b0a510c6b4929d4b3f3bacac7d7c2221
mohdgithub0786 <talibmalik6258@gmail.com>
2023-07-27 22:45:09 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './app/pages/users/candidate/Login1';
+function App() {
+  return (
+    <Login/>
+  );
+}
+
+export default App;
---
+++ src/app/utils/colors.js
@@ -0,0 +1,11 @@
+import react from "react";
+const colors ={
+   textColor:'#CCFFCC',
+//    textColor: 'red',
+   background: '#6973FE',
+   black:'#000000',
+   shadow: '2px solid #CCFFCC',
+   color:"#FFFFFF",
+};
+
+export default colors;
